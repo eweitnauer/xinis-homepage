5b957b22b110f7d4011f79a19cb324f8529caf0f
Erik Weitnauer <eweitnauer@gmail.com>
2012-03-11 21:45:08 +0100

    shows N quadratic divs that smoothly move around

--- main.js
+++ main.js
@@ -1,4 +1,32 @@
+var N=10;
+var colors = d3.scale.category10();
+
+function draw_random(N) {
+  var d=[];
+  for (var i=0; i<N; i++) {
+    d.push({x:Math.random()*540, y:Math.random()*380});
+  }
+  return d;
+}
+
 function init() {
-  d3.select("div#main")
-    .style("top", "100px");
+  d3.select("div#main").selectAll("div")
+    .data(draw_random(N))
+    .enter().append("div")
+    .attr("class", "floating")
+    .style("background-color", function(d,i) {return colors(i)})
+    .style("border-color", function(d,i) {return d3.hsl(colors(i)).darker()});
+  
+  update();
+  
+  setInterval(update, 1500);
+}
+
+function update() {
+  d3.selectAll("div.floating")
+    .data(draw_random(N))
+    .transition()
+    .duration(1000)
+    .style("top", function(d) { return d.y + 'px' })
+    .style("left", function(d) { return d.x + 'px' })
 }
