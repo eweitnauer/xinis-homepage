c1557e296058def3c3ef479cff5f4874585c0b46
Erik Weitnauer <eweitnauer@gmail.com>
2012-03-22 21:51:12 +0100

    moved floating rect example into own directory, added hexagon example

---
+++ 6eck/main.js
@@ -0,0 +1,31 @@
+var svg;
+
+function init() {
+  svg = d3.select("div#background").append("svg");
+  
+  svg.append("polygon")
+     .attr("points", sechseck(100, 120, 150))
+     .attr("fill", "black")
+     .attr("fill-opacity", 0.4)
+     .attr("stroke", "black")
+     .attr("stroke-width", "6px");
+       
+  svg.select("polygon")
+     .transition()
+     .duration(2000)
+     .attr("fill", "red")
+}
+
+/// Gibt die Punkte eines Sechsecks zurueck als "x0,y0 x1,y1 ..."
+function sechseck(a, x, y) {
+  var w3 = Math.sqrt(3);
+  var pts = [[-0.5, -0.5*w3], [0.5, -0.5*w3],
+             [0.5+0.25*w3,0], [0.5, 0.5*w3],
+             [-0.5, 0.5*w3], [-0.5-0.25*w3,0]];
+  for (var i=0; i<6; i++) {
+    pts[i][0] = pts[i][0]*a + x;
+    pts[i][1] = pts[i][1]*a + y;
+  }
+  for (var i=0; i<6; i++) pts[i] = pts[i].join(",");
+  return pts.join(" ");
+}
