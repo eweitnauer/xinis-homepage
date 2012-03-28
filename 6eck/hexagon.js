ae141a818438da8cac3fbf78a5b7b59f6298ed81
Erik Weitnauer <eweitnauer@gmail.com>
2012-03-28 23:25:00 +0200

    subhexagons are now expanded after selecting an hexagon

--- 6eck/hexagon.js
+++ 6eck/hexagon.js
@@ -1,3 +1,9 @@
+var a = 70, ah = a*Math.sqrt(3),
+    gap = a*0.1;
+
+/// positions in hexagon grid
+var places = [[1,0], [0,1], [-1,0], [0,0], [2,0], [2,1], [3,0], [3,-1], [3,1], [2,-1], [2,2], [3,-2], [3,2]];
+
 /// Gibt die Punkte eines Sechsecks zurueck als "x0,y0 x1,y1 ..."
 function hexagon(a, x, y) {
   var w3 = Math.sqrt(3);
@@ -16,6 +22,32 @@ function transform(scale, dx, dy) {
   return "translate(" + dx + "," + dy + ")" + (scale == 1 ? "" : "scale(" + scale + ")");
 }
 
+/// Returns the screen postion of the center of a hexagon being at position i,j.
+/// The hexagons are positioned as follows:
+/// y: 0   o   o   o   o 
+///    0 o   o   o   o   o
+///    1   o   o   o   o
+///    1 o   o   o   o   o
+///  x:  0 1 2 3 4 5 6 7 8
+function hexpos(a, ipos) {
+  var x = ipos[0]*1.5*a;
+  var y = (ipos[1]+0.5*(Math.abs(ipos[0])%2))*Math.sqrt(3)*a;
+  return [x, y];
+}
+
+/// Moves all "g.hexagon" in the svg to the position according to their d.i.
+/// places must be an array of [x,y] arrays and is used to map the d.i values to
+/// positions in the hexagon structure.
+function position_hexagons(duration) {
+  d3.selectAll("svg g.hexagon")
+    .transition()
+    .duration(duration)
+    .attr("transform", function(d) {
+      var pos = hexpos(a+gap, places[d.i]);
+      return transform(1, pos[0], pos[1]);
+    });
+}
+
 /// Returns a random permutation of 0...N-1.
 function get_permutation(N) {
   var a = Array(N); a[0] = 0;
@@ -27,3 +59,45 @@ function get_permutation(N) {
   return a;
 }
 
+/// Call with svg element as this. Will make the element rotate back and forth
+/// between -angle and angle degrees.
+function wiggle_element(angle) {
+  angle = angle || 2;
+  return function() {
+    d3.select(this).transition()
+        .ease('linear')
+        .duration(100)
+        .attr("transform", "rotate("+angle+")")
+        .each("end", wiggle_element(-angle));
+  };
+}
+
+/// Call with svg element as this. Will make the element move to random locations
+/// in [-dist...dist, -dist...dist].
+function float_element(dist) {
+  dist = dist || 3;
+  return function() {
+    d3.select(this).transition()
+        .ease("linear")
+        .duration(500+Math.random()*100)
+        .attr("transform", transform(1, Math.random()*2*dist-dist, Math.random()*2*dist-dist))
+        .each("end", float_element(dist));
+  };
+}
+
+/// Starts / stops floating and sets d.floating attribute for all ".floating"
+/// elements in the svg. When stopping the floating, the elements are moved
+/// back to 0,0 in duration ms.
+function setFloating(do_float, duration) {
+  if (do_float) {
+    d3.selectAll("svg .floating")
+      .each(function(d) { d.floating = true; })
+      .each(float_element());
+  } else {
+    d3.selectAll("svg .floating")
+      .each(function(d) { d.floating = false; })
+      .transition()
+      .duration(duration)
+      .attr("transform", transform(1, 0, 0));
+  }
+}
--- 6eck/main.js
+++ 6eck/main.js
@@ -1,20 +1,28 @@
 /* Ablauf:
 1. 4 grosse Sechecke in der Mitte (wackeln)
-2. bei Click: das geklickte wird markiert, wandert nach oben, alle vier werden
+2. bei Click: das geklickte wird markiert, wandert nach rechts, alle vier werden
    kleiner und bewegen sich an den linken Rand (wackeln nicht mehr)
 3a. bei erneutem Click auf ausgewaehltes: wandern zurueck in die Mitte
-3b.                    auf nicht ausgewaehltes: markierung wechselt, 6eck nach oben
+3b.                    auf nicht ausgewaehltes: markierung wechselt, 6eck nach rechts
 */
 
 var svg;
-var a = 70, ah = a*Math.sqrt(3),
-    gap = 0.1;
-var places = [[1,0], [0,1], [-1,0], [0,0]];
-var hexes = [{a: a, place: 0, label: "Illustrationen"},
-             {a: a, place: 1, label: "Kunst"},
-             {a: a, place: 2, label: "Logos"},
-             {a: a, place: 3, label: "Kataloge"}];
-
+var scaling = 0.8;
+var hexes = [{i: 0, label: "Illustrationen", children:
+               [{i: 4, label: "I1"}
+               ,{i: 5, label: "I2"}
+               ,{i: 6, label: "I3"}
+               ,{i: 7, label: "I4"}
+               ,{i: 8, label: "I5"}
+               ,{i: 9, label: "I6"}
+               ,{i: 10, label: "I7"}
+               ,{i: 11, label: "I8"}
+               ,{i: 12, label: "I9"}
+               ]
+             },
+             {i: 1, label: "Kunst", children: []},
+             {i: 2, label: "Logos", children: []},
+             {i: 3, label: "Kataloge", children: []}];
 var w, h;
 var t=Date.now();
 
@@ -28,7 +36,6 @@ function init() {
   
   svg.append("g")
     .attr("class", "main");
-  move_to_center(0);
     
   var glinks = svg.select("g.main")
     .selectAll("g.hexagon")
@@ -36,112 +43,113 @@ function init() {
     .append("g")
     .attr("class", "hexagon")
     .append("g")
-    .attr("class", "inner")
+    .attr("class", "floating")
     .on("mouseover", function() {d3.select(this).transition().duration(0);})
-    .on("mouseout", function() {d3.select(this).each(move())})
+    .on("mouseout", function(d) { if (d.floating) d3.select(this).each(float_element())})
     .on("click", clicked_main);
+    
+  glinks.append("polygon")
+   .attr("class", "main")
+   .attr("points", function(d) { return hexagon(a, 0, 0); })
+   .attr("fill-opacity", 0.4)
+   .attr("stroke", "black")
+   .attr("stroke-width", "4px");
 
-  position_hexagons(0);
-  
   glinks.append("text")
    .attr("alignment-baseline", "middle")
    .attr("text-anchor", "middle")
    .text(function(d) { return d.label; });
-  
-  glinks.append("polygon")
-   .attr("class", "main")
-   .attr("points", function(d) { return hexagon(d.a, 0, 0); })
-   .attr("fill", "black")
-   .attr("fill-opacity", 0.4)
-   .attr("stroke", "black")
-   .attr("stroke-width", "4px");
-      
-  svg.selectAll("g.inner")
-    .each(move());
+
+  move_to_center(0);
+  position_hexagons(0, a+gap);
+  setFloating(true);
 }
 
 function move_to_center(duration) {
   svg.select("g.main")
     .transition()
     .duration(duration)
-    .attr("transform", transform(1,w/2,(h-ah-gap*a)/2));
+    .attr("transform", transform(1,w/2,(h-ah-gap)/2));
+  // start floating
+  setFloating(true);
 }
 
 function move_to_left(duration) {
   d3.select("g.main")
     .transition()
     .duration(duration)
-    .attr("transform", transform(0.6, (1.5+gap)*a, (h-ah-gap*a)/2));
-}
-
-function position_hexagons(duration) {
-  d3.selectAll("g.hexagon")
-    .transition()
-    .duration(duration)
-    .attr("transform", function(d) {
-      var pos = hexpos(d.a, places[d.place]);
-      return transform(1, pos[0], pos[1]);
-    });
+    .attr("transform", transform(scaling, scaling*(3*a+gap), (h-ah-gap)/2));
+  // stop floating
+  setFloating(false, duration);
 }
 
 function clicked_main(d) {
+  // was the already selected hexagon clicked?
   if (d.selected) {
+    // yes, so deselect it and move all hexagons back to center
     d.selected = false;
+    d3.select(this).select("polygon").attr("class", "");
     move_to_center(1000);
-    // update selection class
-    d3.selectAll("polygon")
-      .attr("class", function(d) { return d.selected ? "selected" : "" });
     return;
   }
-  // unselect all hexagons
-  for (var i=0; i<hexes.length; i++) {
-    hexes[i].selected = false;
-  }
-  // select current one
+  
+  // a new hexagon was clicked!
+  // select new one...
+  for (var i=0; i<hexes.length; i++) hexes[i].selected = false;
   d.selected = true;
-  // update selection class
   d3.selectAll("polygon")
     .attr("class", function(d) { return d.selected ? "selected" : "" });
-  
-  if (d.place != 0) {
+  // set its index to 0...
+  if (d.i != 0) {
     for (var i=0; i<hexes.length; i++) {
-      if (hexes[i].place == 0) {
-        hexes[i].place = d.place;
-        d.place = 0;
+      if (hexes[i].i == 0) {
+        hexes[i].i = d.i;
+        d.i = 0;
         break;
       }
     }
     position_hexagons(1000);
   }
+  // and move all hexagons to the left
   move_to_left(1000);
-  d3.selectAll("g.inner")
-    .transition()
-    .duration(1000)
-    .attr("transform", transform(1, 0, 0));
+  setTimeout(function() { expand_inner_links(d) }, 1000);
 }
 
-function wiggle(angle) {
-  return function() {
-    d3.select(this).transition()
-        .ease('linear')
-        .duration(100)
-        .attr("transform", "rotate("+angle+")")
-        .each("end", wiggle(-angle));
-  };
-}  
+function expand_inner_links(d) {
+  var hs = d3.select("svg g.main").selectAll("g.hexagon")
+    .data(hexes.concat(d.children));
 
-function move() {
-  return function() {
-    d3.select(this).transition()
-        .ease("linear")
-        .duration(500+Math.random()*100)
-        .attr("transform", transform(1, Math.random()*6-3, Math.random()*6-3))
-        .each("end", move());
-  };
-}  
+  var pos = hexpos(a+gap, places[0]);
+  
+  var hse = hs.enter()
+    .append("g", "g.hexagon")
+    .attr("class", "hexagon")
+    .attr("transform", transform(1, pos[0], pos[1]))
+    .attr("z-index", -100)
+    .append("g")
+    .attr("class", "floating")
+    .on("mouseover", function() {d3.select(this).transition().duration(0);})
+    .on("mouseout", function(d) { if (d.floating) d3.select(this).each(float_element())})
+//    .on("click", clicked_sub);
+  
+  hse.append("text")
+   .attr("alignment-baseline", "middle")
+   .attr("text-anchor", "middle")
+   .text(function(d) { return d.label; });
+  
+  hse.append("polygon")
+   .attr("class", "main")
+   .attr("points", function(d) { return hexagon(a, 0, 0); })
+   .attr("fill", "black")
+   .attr("fill-opacity", 0.4)
+   .attr("stroke", "black")
+   .attr("stroke-width", "4px");
 
-function hexpos(a, ipos) {
-  var x = ipos[0]*1.5*(a+gap*a);
-  var y = (ipos[1]+0.5*(Math.abs(ipos[0])%2))*Math.sqrt(3)*(a+gap*a);
-  return [x, y];
+  position_hexagons(1000, a+gap);
+  
+  hs.exit()
+    .transition()
+    .duration(1000)
+    .attr("transform", transform(1, pos[0], pos[1]))
+    .remove();
 }
